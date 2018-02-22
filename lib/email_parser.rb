f1c0d10ae64f0555e071574e5b21b8de7d7506cc
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-22 23:54:58 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailParser
   end
 
   def parse
-    @@email_result = @emails.gsub(/,\s/, ' ').split(" ")
+    @@email_result = @emails.gsub(/,\s/, ' ').split(" ").unique
     @@email_result
 
   end
